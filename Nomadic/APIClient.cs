ffa9f23380c23e3953944ad226f0203956f9b0da
Aleksander Johansen <aleksander.johans@gmail.com>
2021-09-15 01:55:36 +0200

    Add files via upload

---
+++ Nomadic/APIClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Nomadic
+{
+    class APIClient
+    {
+        public static async Task SignInUser(string email, string password)
+        {
+            using var client = new HttpClient();
+            var response = await client.GetStringAsync("https://nomadicserver.000webhostapp.com/scripts/test.php");
+            Trace.WriteLine(response); 
+            if(response == "hei fra nomadic server")
+            {
+                Trace.WriteLine("Det fungerer");
+            }
+            else
+            {
+                Trace.WriteLine("Det fungerer ikke");
+            }
+        }
+    }
+
+    
+}
---
+++ Nomadic/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nomadic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void btnSignInClick(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            Trace.WriteLine(email);
+            await APIClient.SignInUser(email, password);
+        }
+    }
+}
